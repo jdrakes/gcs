22c7259b7481386d7388c95fdd241b097100a261
Chris Freeze <chrstnfreeze@gmail.com>
2019-09-21 15:44:34 -0500

    add content-type conditional decoding

--- lib/gcs/client/httpoison.ex
+++ lib/gcs/client/httpoison.ex
@@ -8,13 +8,20 @@ defmodule GCS.Client.HTTPoison do
     |> handle_response()
   end
 
-  defp handle_response({:ok, %Response{status_code: status, body: body}})
+  defp handle_response({:ok, %Response{status_code: status, body: body, headers: headers}})
        when status in [200, 204],
-       do: {:ok, Jason.decode(body)}
+       do: {:ok, decode_body(headers, body)}
 
   defp handle_response({:ok, %Response{status_code: _code}}),
     do: {:error, :unexpected_gcs_response}
 
   defp handle_response({:ok, %Error{reason: :timeout}}), do: {:error, :gateway_timeout}
   defp handle_response({:ok, %Error{reason: reason}}), do: {:error, reason}
+
+  defp decode_body(headers, body) do
+    case List.keyfind(headers, "Content-Type", 0) do
+      "application/json" -> Jason.decode(body)
+      _ -> body
+    end
+  end
 end
